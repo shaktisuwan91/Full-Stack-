09707ccdc162b628185fe5fdc479aa8b8d7a8820
shaktisuwan91 <shaktisuwan91@gmail.com>
2018-08-15 12:24:40 +0530

    FullStack

---
+++ Django Project/music/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+
+from . models import Albums
+admin.site.register(Albums)
+# Register your models here.
---
+++ Django Project/music/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h1>This is Music App home page")
+
+# Create your views here.
